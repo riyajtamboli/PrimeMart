fdef9a60a65874d88c7f88e6492e987aa4584c90
Riyaj Tamboli <tambolir97@gmail.com>
2026-07-25 14:16:04 +0530

    Added Wishlist entity

---
+++ src/main/java/com/primemart/entity/Wishlist.java
@@ -0,0 +1,28 @@
+package com.primemart.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "wishlists")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Wishlist extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
